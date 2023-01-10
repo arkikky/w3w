e9ba8fa896b4b3bffb7f33ccc269e67d97cb82f7
arkikky <112843504+arkikky@users.noreply.github.com>
2023-01-10 13:34:57 +0700

    Update
    awd

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [process.env.NEXT_PUBLIC_DOMAIN],
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
