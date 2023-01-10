4d76f34e8ca4741dcc07044339809c534ec210e8
arkikky <112843504+arkikky@users.noreply.github.com>
2023-01-10 09:56:29 +0700

    Update next.config & sitemap
    #update next.config & sitemap

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
     domains: [process.env.NEXT_PUBLIC_DOMAIN],
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60,
-    disableStaticImages: true,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
