1eb7735d734fedc291735c8f252771594fa9cf27
arkikky <112843504+arkikky@users.noreply.github.com>
2023-01-16 09:21:39 +0700

    Update w3w, sitemap config & remove unused file Images
    #update w3w, sitemap config & remove unused file Images

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 
+const siteURL =
+  process.env.NEXT_PUBLIC_SITE_URL || "https://www.coinvestasi.com/";
+
 const nextSitemapConfig = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   changefreq: "daily",
   priority: 0.7,
   generateRobotsTxt: true,
+  sitemapSize: 1000,
   robotsTxtOptions: {
+    additionalSitemaps: [`${siteURL}server-sitemap.xml`],
     policies: [
       {
         userAgent: "*",
